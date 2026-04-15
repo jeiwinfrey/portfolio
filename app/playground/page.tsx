816d9acc8aabd8a8be46d4f52ed41e1319257ac6
jeiwinfrey <jeiwinfreyulep12@gmail.com>
2026-04-15 09:55:11 +0800

    chore: add pages for proper routing

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,9 +1,9 @@
 import NavBar from "@/components/navbar";
 
-export default function Page() {
+export default function PlaygroundPage() {
   return (
-    <div className="max-w-xl">
+    <div>
       <NavBar />
     </div>
-  );
+  )
 }
